157f0423d62b26c0504b2e195a0ef66a72088492
utkarshqw <utkarshup75@gmail.com>
2023-01-09 13:52:44 +0530

    day2

---
+++ frontend/src/Pages/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeroSlider from '../components/HeroSlider'
+import Slider from '../components/Slider'
+
+const Homepage = () => {
+  return (
+    <>
+    {/* <HeroSlider/> */}
+    <Slider/>
+    </>
+  )
+}
+
+export default Homepage
---
+++ frontend/src/components/Slider.jsx
@@ -0,0 +1,39 @@
+import { Box, Button } from '@chakra-ui/react'
+import React from 'react'
+import { useState } from 'react'
+import { useRef } from 'react'
+import { useEffect } from 'react'
+import styles from "../Styles/Slider.module.css"
+
+const Slider = () => {
+  
+
+
+  
+  
+  
+ 
+
+  return (
+    <>
+  
+    <input  type="radio" name='rdo' id={styles.r1} />
+    <input   type="radio" name='rdo' id={styles.r2} />
+    <input   type="radio" name='rdo' id={styles.r3} />
+    <input   type="radio" name='rdo' id={styles.r4} />
+
+    <Box
+    display={"flex"}
+    overflow="hidden"
+    id={styles.first}
+    >
+      <img src="slider1.jpg" id={styles.fimg} />
+      <img src="slider2.jpg"  />
+      <img src="slider3.jpg"  />
+      <img src="slider4.jpg"  />
+    </Box>
+    </>
+  )
+}
+
+export default Slider
