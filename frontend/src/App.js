7a17b0c57d7614101dfdab6cd16b25939f74632e
utkarshqw <utkarshup75@gmail.com>
2023-02-21 23:03:46 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
 import { Box, Button } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 import Allroutes from "./routes/Allroutes";
 
 function App() {
   
+  
   return (
-    <Box bg="rgb(234,237,237)" className="App">
+    <Box fontSize={40} bg="rgb(234,237,237)" className="App">
       
       <Allroutes />
+      
    </Box>
   );
 }
